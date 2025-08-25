64cd7dbcac8608647d8639a35211cff5ab631df7
Tushar Nagar <tushar.nagar.2@gmail.com>
2025-08-25 10:54:06 +0530

    fix catalog

--- src/notebook_1.py
+++ src/notebook_1.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 # setup_unity_catalog.py
-spark.sql(f"CREATE CATALOG IF NOT EXISTS {spark.conf.get('taskCatalog')}")
+# spark.sql(f"CREATE CATALOG IF NOT EXISTS {spark.conf.get('taskCatalog')}")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {spark.conf.get('taskCatalog')}.{spark.conf.get('taskSchema')}")
