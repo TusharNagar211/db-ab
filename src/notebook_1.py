1f30d1973a270d389c8eaf4477777d25ad9c8636
Tushar Nagar <tushar.nagar.2@gmail.com>
2025-09-03 14:21:26 +0530

    release tested

--- src/notebook_1.py
+++ src/notebook_1.py
@@ -1,4 +1,6 @@
 # Databricks notebook source
 # setup_unity_catalog.py
-# spark.sql(f"CREATE CATALOG IF NOT EXISTS {spark.conf.get('taskCatalog')}")
-spark.sql(f"CREATE SCHEMA IF NOT EXISTS {spark.conf.get('taskCatalog')}.{spark.conf.get('taskSchema')}")
+spark.sql("CREATE CATALOG IF NOT EXISTS bronze_tbd MANAGED LOCATION 'abfss://unity-catalog-storage@dbstoragezwbsqsptedmwu.dfs.core.windows.net/4091704180100433'")
+spark.sql("CREATE SCHEMA IF NOT EXISTS bronze_tbd.my_schema")
+
+spark.sql("GRANT USE_CATALOG, USE_SCHEMA, APPLY_TAG ON CATALOG bronze_tbd TO `hemali.riaan@gmail.com`")
